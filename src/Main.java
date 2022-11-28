f0172e2a922e782643004124a92d98433707877d
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-11-28 21:36:08 +0500

    Задание 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание 1.1
+        int deposit = 0;
+        int amount = 15_000;
+        int goal = 2_459_000;
+        int month = 0;
+
+        while (deposit <= goal){
+            deposit += amount;
+            month++;
+
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + deposit + " рублей");
+        }
+
     }
 }
